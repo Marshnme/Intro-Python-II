e2275223cbcf5eef01091b5f44ac9023cfac510a
Joshua Holtsclaw <jildsmiles@yahoo.com>
2020-02-13 19:22:35 -0500

    showing item per room. Lots of work left

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,30 @@
 from room import Room
 from player import Player
+import random
 # Declare all the rooms
 
+potential = ["knife","chestplate","gold","skull"]
+pick = random.choice(potential)
+pick2 = random.choice(potential)
+pick3 = random.choice(potential)
+pick4 = random.choice(potential)
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons",f'loot: {pick}'),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""",f'loot: {pick2}'),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""",f'{pick3}'),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""",f'{pick4}'),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""","Treasure!"),
 }
 
 
--- src/room.py
+++ src/room.py
@@ -2,9 +2,10 @@
 # description attributes.
 
 class Room:
-    def __init__(self,name,description):
+    def __init__(self,name,description,item):
         self.name = name
         self.description = description
+        self.item = item
     def __str__(self):
-        return f'{self.name}, {self.description}'.format(self=self)
+        return f'{self.name}, {self.description}, {self.item}'.format(self=self)
         
